cfe2a2a171974bb86d87f780c00ce46c5811d970
MatsM16 <matdyr23@gmail.com>
2019-01-16 18:40:59 +0100

    Updated readme

---
+++ Program/Program.ino
@@ -0,0 +1,8 @@
+/*
+  This is the OS for the 3STKA TOF Self Driving Car
+
+  As well as testing the limits of what can be accomplished with simple sensors and moroty,
+  this project will obey to object-oriented design and dependency injection.
+  
+  Author: Vetle Strandtun
+*/
