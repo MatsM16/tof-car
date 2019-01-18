be9eb084f8e310fec64cee586d137f2ec685a4d5
MatsM16 <matdyr23@gmail.com>
2019-01-18 17:37:02 +0100

    Added Mats as author of code base

--- Program/Program.ino
+++ Program/Program.ino
@@ -4,5 +4,5 @@
   As well as testing the limits of what can be accomplished with simple sensors and moroty,
   this project will obey to object-oriented design and dependency injection.
   
-  Author: Vetle Strandtun
+  Author: Vetle Strandtun, Mats Omland Dyrøy
 */
