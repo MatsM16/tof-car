f577801aa65f196c402b9e1468d9c7de301b0b77
MatsM16 <matdyr23@gmail.com>
2019-01-18 17:36:44 +0100

    Added max power and ability to set max power

--- Program/Engine.ino
+++ Program/Engine.ino
@@ -1,17 +1,30 @@
+/*
+ * An object-oriented abstraction of a motor
+*/
 class Engine
 {
   private:
     int pin;
+    byte maxPower;
 
   public:
     Engine(int pin)
     {
       this->pin = pin;
       pinMode(pin, OUTPUT);
+
+      maxPower = 255;
+    }
+
+    void SetMaxPower(byte power)
+    {
+      maxPower = power;
     }
 
     void SetPower(byte power)
     {
+      power = min(power, maxPower);
+      
       analogWrite(pin, power);
     }
 };
