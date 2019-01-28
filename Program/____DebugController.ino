ef99963d2a806c45b3f5dca983a90daf495b017b
MatsM16 <matdyr23@gmail.com>
2019-01-28 13:38:41 +0100

    Added debug mode

--- Program/____DebugController.ino
+++ Program/____DebugController.ino
@@ -7,11 +7,43 @@ class DebugController : public ICarController
     SensorReader* sensors;
     Car* car;
 
+    int debug_delaySecconds;
+    byte debug_minPower;
+    byte debug_maxPower;
+    int debug_minAngle;
+    int debug_maxAngle;
+
+    void Delay()
+    {
+      delay(1000 * debug_delaySecconds);
+    }
+
   public:
     void Init(Config* cfg, SensorReader* sensors, Car* car)
     {
       this->sensors = sensors;
       this->car = car;
+
+      if (cfg != NULL)
+      {
+        debug_delaySecconds = cfg->GetInt("DEBUG_DELAY_SECCONDS", 5);
+        
+        debug_minPower = (byte)cfg->GetInt("DEBUG_POWER_MIN", 0);
+        debug_maxPower = (byte)cfg->GetInt("DEBUG_POWER_MAX", 255);
+        
+        debug_minAngle = cfg->GetInt("DEBUG_ANGLE_MIN", -90);
+        debug_maxAngle = cfg->GetInt("DEBUG_ANGLE_MAX", 90);
+      }
+      else
+      {
+        debug_delaySecconds = 5;
+        
+        debug_minPower = 0;
+        debug_maxPower = 255;
+        
+        debug_minAngle = -90;
+        debug_maxAngle = 90;
+      }
     }
 
     void Update()
@@ -28,13 +60,10 @@ class DebugController : public ICarController
       
       delete readout;
 
-      car->Update(0, -90);
-
-      delay(1000 * 5);
-
-      car->Update(255, 90);
-
-      delay(1000 * 5);
+      car->Update(debug_minPower, debug_minAngle);
+      Delay();
+      car->Update(debug_maxPower, debug_maxAngle);
+      Delay();
     }
 
     void Dispose()
